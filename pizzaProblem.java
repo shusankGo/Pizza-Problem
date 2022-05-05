4f00958bba7a9b27ecaf0251611023d58eb90442
Shusank Dahal <shushank.dahal@gokloud.io>
2022-05-05 17:26:32 +0545

    Added pizzaProblem.java with the solution to the problem

---
+++ pizzaProblem.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    static void cutPizza(int n) {
+        // for case no 1
+        System.out.print((360 % n == 0) ? "1 " : "0 ");
+        // for case no 2
+        System.out.print((n <= 360) ? "1 " : "0 ");
+        // for case no 3
+        System.out.print((((n * (n + 1)) / 2) <= 360) ? "1 " : "0 ");
+    }
+
+    public static void main(String args[]) {
+        int n = 11; // intilization of n
+        cutPizza(n);
+    }
+}
